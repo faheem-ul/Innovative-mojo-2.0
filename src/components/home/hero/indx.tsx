9dd81479da093083db882b7baaf2e709b8e96e4d
Faheem <seebiz.gpt0623.dev56@gmail.com>
2025-02-25 19:21:30 +0500

    animations are done

--- src/components/home/hero/indx.tsx
+++ src/components/home/hero/indx.tsx
@@ -1,24 +1,79 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import Text from "@/components/ui/Text";
+import TypeWriterText from "@/components/ui/TypeWriterText";
+
 import video from "@/public/images/home/hero-video.png";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const HomeHero = () => {
+  const imageRef = useRef(null);
+  const wrapperRef = useRef(null); // Parent container for perspective
+
+  useEffect(() => {
+    if (imageRef.current && wrapperRef.current) {
+      gsap.set(wrapperRef.current, { perspective: 1200 }); // Set perspective on parent
+
+      gsap.fromTo(
+        imageRef.current,
+        {
+          rotateX: 18,
+          // opacity: 0.8,
+        },
+        {
+          rotateX: 0, // End tilt
+          opacity: 1,
+          duration: 1.5,
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: imageRef.current,
+            start: "top center",
+            end: "center center",
+            scrub: true,
+          },
+        }
+      );
+    }
+  }, []);
+
   return (
     <div className="w-full h-full mt-[76px] px-5">
       <div className="w-full h-full flex justify-center items-center">
         <div className="w-full max-w-[1236px]">
-          <Text as="h1">
-            Innovative <span className="text-gold">Mojo</span>
+          <Text as="h1" className="flex gap-10 mob:flex-col mob:gap-0">
+            Innovative{" "}
+            <span>
+              <TypeWriterText AnimatatedWords={["Mojo"]} />
+            </span>
           </Text>
+
           <Text className="text-[32px] font-semibold leading-[38px] mb-[121px] mob:mb-[60px] mob:text-center mob:mt-1">
             Where AI meets creativity for unstoppable{" "}
             <span className="text-gold"> brand growth</span>
           </Text>
+        </div>
+      </div>
+
+      {/* Parent wrapper with perspective applied */}
+      <div
+        className="perspective-container relative w-full overflow-hidden flex justify-center mb-[200px] mob:mb-[60px]"
+        ref={wrapperRef}
+      >
+        <div
+          className="relative overflow-hidden transition-all duration-500 z-10 h-auto w-full"
+          ref={imageRef}
+        >
           <Image
             src={video}
-            alt=""
-            className="w-full min-h-[800px] mob:min-h-full mb-[174px] mob:mb-[60px]"
+            alt="Animated Hero Video"
+            className="w-full object-cover max-w-[1232px] mx-auto"
+            style={{ aspectRatio: "16/9" }}
           />
         </div>
       </div>
